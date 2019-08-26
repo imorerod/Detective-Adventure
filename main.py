5fdbe2fae11f9b69df5bc72db58e2270c38e3836
imorerod <isaac.morerod@gmail.com>
2019-08-26 17:28:54 -0500

    created select_choice function

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import time
 
 
 def start(inv):
-    print('\nYou are in front of the house. What are you doing?')
-    print('\n1: Open the door')
-    print('2: Examine the mailbox')
-    print('3: Look under the mat')
-    print('4: Look through the window')
+    print('\nYou are in front of the house. What do you do?')
+    print('\n1: Open the door.')
+    print('2: Examine the mailbox.')
+    print('3: Look under the mat.')
+    print('4: Look through the window.')
+    print('Quit?')
 
     selection_list = ['1', '2', '3', '4']
     choice = select_choice(selection_list)
@@ -14,9 +15,18 @@ def start(inv):
 
 
 
+
+
 def select_choice(selection_list):
     choice = input('\n? ')
-    if
+    if choice in selection_list
+        return choice
+    elif choice.lower().strip() == 'quit':
+        exit(0)
+    else:
+        print('Invalid answer.')
+        return select_choice(selection_list)
+
 
 
 
