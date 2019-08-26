288b5bf262f9b7931f385380b690ca205ad1db5e
imorerod <isaac.morerod@gmail.com>
2019-08-26 17:49:40 -0500

    choice 2 options complete

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 import time
 
 
 def start(inv):
-    print('\nYou are in front of the house. What do you do?')
+    print('\nYou are in front of the house. What do you want to do?')
     print('\n1: Open the door.')
     print('2: Examine the mailbox.')
     print('3: Look under the mat.')
@@ -12,14 +13,41 @@ def start(inv):
     selection_list = ['1', '2', '3', '4']
     choice = select_choice(selection_list)
 
+    if choice == '1':
+        if 'key' in inv:
+            os.system('clear')
+            print('You open the door with the key.')
+            inside(inv)
+        else:
+            os.system('clear')
+            print('The door is locked. You will need a key.')
+            time.sleep(2)
+            start()
+    elif choice == '2':
+        os.system('clear')
+        print('You open the mailbox and find a hard drive.')
+        time.sleep(1)
+        print('What do you want to do?')
+        print('\n1: Take the hard drive and close the mailbox.')
+        print('2: Close the mailbox.')
 
+        selection_list = ['1', '2']
+        choice = select_choice(selection_list)
 
-
+        if choice == '1':
+            inv.append('hard drive')
+            print('\nYou take the hard drive and close the mailbox.')
+            time.sleep(2)
+            start()
+        elif choice = '2'
+            print('\nYou leave the hard drive and close the mailbox.')
+            time.sleep(2)
+            start()
 
 
 def select_choice(selection_list):
     choice = input('\n? ')
-    if choice in selection_list
+    if choice in selection_list:
         return choice
     elif choice.lower().strip() == 'quit':
         exit(0)
@@ -28,8 +56,10 @@ def select_choice(selection_list):
         return select_choice(selection_list)
 
 
+def inside(inv):
+    print('\nYou are inside the house.')
 
 
-
+os.system('clear')
 inv = ['']
 start(inv)
