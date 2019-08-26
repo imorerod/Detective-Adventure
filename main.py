7189b6b609255f5eb21d31cd27f9773736b1731f
imorerod <isaac.morerod@gmail.com>
2019-08-26 17:24:06 -0500

    initial options given

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import time
 
+
 def start(inv):
-    print('You are in front of the house. What are you doing?')
+    print('\nYou are in front of the house. What are you doing?')
+    print('\n1: Open the door')
+    print('2: Examine the mailbox')
+    print('3: Look under the mat')
+    print('4: Look through the window')
+
+    selection_list = ['1', '2', '3', '4']
+    choice = select_choice(selection_list)
 
 
+
+
+def select_choice(selection_list):
+    choice = input('\n? ')
+    if
+
+
+
+
+inv = ['']
+start(inv)
